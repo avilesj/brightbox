1b5ee225690a99049b929ab41912be1c564e7151
Jose Aviles <jlapmail@gmail.com>
2018-11-11 23:05:54 -0400

    Some css

--- src/main/java/com/javiles/brightbox/repositories/PhraseRepositoryImpl.java
+++ src/main/java/com/javiles/brightbox/repositories/PhraseRepositoryImpl.java
@@ -17,6 +17,11 @@ public class PhraseRepositoryImpl implements PhraseRepository
         this.messages.add("Te amo Dayrine <3");
         this.messages.add("Best 2 and half years of my life <3");
         this.messages.add("Believe that the sun will shine tomorrow");
+        this.messages.add("You are my forever");
+        this.messages.add("You and I, shake and fries.");
+        this.messages.add("Picture this: A dog, a house, a... I think you are happy with the dog :)");
+        this.messages.add("We should totally travel to europe someday");
+        this.messages.add("I love that you love my food, did you know that?");
 
         this.phrases = new ArrayList<>();
 
